2cc03bf56bda15cc8e1f7aa0bf581c8fa2f39deb
sengchul.park <ochestra365@naver.com>
2024-01-30 14:11:13 +0900

    [FUNC] pdf image to text

--- PDF_OCR/Form1.Designer.cs
+++ PDF_OCR/Form1.Designer.cs
@@ -31,36 +31,59 @@ namespace PDF_OCR
         {
             this.rtbOcr = new System.Windows.Forms.RichTextBox();
             this.btnConvert = new System.Windows.Forms.Button();
+            this.picMain = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
             this.SuspendLayout();
             // 
             // rtbOcr
             // 
-            this.rtbOcr.Location = new System.Drawing.Point(3, 2);
+            this.rtbOcr.Location = new System.Drawing.Point(3, 12);
             this.rtbOcr.Name = "rtbOcr";
-            this.rtbOcr.Size = new System.Drawing.Size(494, 447);
+            this.rtbOcr.Size = new System.Drawing.Size(515, 708);
             this.rtbOcr.TabIndex = 0;
             this.rtbOcr.Text = "";
             // 
             // btnConvert
             // 
-            this.btnConvert.Location = new System.Drawing.Point(517, 46);
+            this.btnConvert.Location = new System.Drawing.Point(1072, 46);
             this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(271, 355);
+            this.btnConvert.Size = new System.Drawing.Size(324, 368);
             this.btnConvert.TabIndex = 1;
             this.btnConvert.Text = "Convert";
             this.btnConvert.UseVisualStyleBackColor = true;
             this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
             // 
+            // picMain
+            // 
+            this.picMain.Location = new System.Drawing.Point(524, 12);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(521, 708);
+            this.picMain.TabIndex = 2;
+            this.picMain.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1072, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1429, 772);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.picMain);
             this.Controls.Add(this.btnConvert);
             this.Controls.Add(this.rtbOcr);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +91,8 @@ namespace PDF_OCR
 
         private System.Windows.Forms.RichTextBox rtbOcr;
         private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.PictureBox picMain;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- PDF_OCR/Form1.cs
+++ PDF_OCR/Form1.cs
@@ -3,43 +3,115 @@ using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 using Tesseract;
+using Ghostscript.NET;
+using Ghostscript.NET.Rasterizer;
 namespace PDF_OCR
 {
     public partial class Form1 : Form
     {
         /*
             1. reference URL : https://virusheo.blogspot.com/2023/04/230422.html
-            2. License : Aphace-2.0
+            2. License 1: Aphace-2.0[Conversion Of image to text]
+            3. License 2: GhostScript[AGPL(https://www.gnu.org/licenses/agpl-3.0.html)]
+             -> https://www.oss.kr/oss_license_qna/show/222bcc65-ddc5-48e5-8d98-c162ef9f9598?page=80
+        ==================================================================================================
+            1. fileName should be written by English.
+            2. The Save Path should be Startup Path of Application.
+        ===================================================================================================
+        logic
+        1. Choose pdf.
+        2. Convert pdf to image(GhostScript)
+        3. Convert image to text(tesseract)
         */
         public Form1()
         {
             InitializeComponent();
             this.CenterToScreen();
+            this.picMain.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
         private void btnConvert_Click(object sender, EventArgs e)
         {
-            string startUpPath = Directory.GetCurrentDirectory() + "\\";
-            string imageName = "c_lang.bmp";
-
-            this.rtbOcr.Text = string.Empty;
-
-            string result = GetText(startUpPath + imageName, "kor+eng");
-            if (!string.IsNullOrEmpty(result))
+            OpenFileDialog dialog = new OpenFileDialog();
+            picMain.Image = null;
+            string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "IMAGE");
+            if (dialog.ShowDialog() == DialogResult.OK)
             {
-                this.rtbOcr.Text = result;
+                #region extension check
+                this.rtbOcr.Text = string.Empty;
+                if (!Path.GetExtension(dialog.FileName).ToUpper().Replace(".", string.Empty).Equals("PDF"))
+                {
+                    MessageBox.Show("Please select PDF");
+                    return;
+                }
+                #endregion
+                #region folder and file check
+                DirectoryInfo dir = new DirectoryInfo(imagePath);
+                if (!dir.Exists) { dir.Create(); }
+                FileInfo[] infos = dir.GetFiles();
+                if (infos.Length > 0)
+                {
+                    foreach (FileInfo file in infos)
+                    {
+                        if (File.Exists(file.FullName))
+                        {
+                            try
+                            {
+                                GC.Collect();
+                                GC.WaitForPendingFinalizers();
+                                File.Delete(file.FullName);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"TIME : {DateTime.Now}. CONTENT : {ex}");
+                            }
+                        }
+                    }
+                }
+                #endregion
+                MakePDFtoImage(dialog.SafeFileName, int.TryParse(this.textBox1.Text, out int page) ? page : 1);
+                string result = GetText(Path.Combine(imagePath,"temp.png"), "kor+eng");
+                if (!string.IsNullOrEmpty(result))
+                {
+                    this.rtbOcr.Text = result;
+                }
+                Bitmap bitmap = new Bitmap($@"{imagePath}\temp.png");
+                picMain.Image = bitmap;
             }
         }
 
+        private bool MakePDFtoImage(string _pdfPath, int _page)
+        {
+            try
+            {
+                GhostscriptVersionInfo gvi = new GhostscriptVersionInfo($@"{Directory.GetCurrentDirectory()}\gsdll32.dll");
+                using (GhostscriptRasterizer rasterizer = new GhostscriptRasterizer())
+                {
+                    rasterizer.Open(_pdfPath, gvi, false);
+                    if (rasterizer.PageCount < _page) { return false; }
+                    rasterizer.GetPage(200, _page).Save(Path.Combine(Directory.GetCurrentDirectory(), "IMAGE", "temp.png"));
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
+        }
+        /// <summary>
+        /// 이미지에 적힌 텍스트를 분석하는 메서드
+        /// </summary>
+        /// <param name="_filePath">파일 경로</param>
+        /// <param name="_language">변환할 언어</param>
+        /// <returns>변환이 완료된 언어</returns>
         private string GetText(string _filePath, string _language = "eng")
         {
-
-
             string result = string.Empty;
             try
             {
                 Bitmap img = new Bitmap(_filePath);
-                var ocr = new TesseractEngine("./tessdata", _language, EngineMode.Default);
+                TesseractEngine ocr = new TesseractEngine("./tessdata", _language, EngineMode.Default);
                 result = ocr.Process(img).GetText();
                 return result;
             }
